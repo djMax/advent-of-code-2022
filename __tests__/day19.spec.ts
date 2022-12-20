9732949afc9a853a077fc0b5e03c7cdf52a658f4
Max Metral <max@pyralis.com>
2022-12-20 16:03:36 -0500

    Day 20. GH Copilot picks * instead of + and I don't notice

--- __tests__/day19.spec.ts
+++ __tests__/day19.spec.ts
@@ -1,5 +1,7 @@
 import { spawn, Pool, Worker } from 'threads';
-import { Blueprint, getLines, log, Resource } from '../src';
+import {
+  Blueprint, getLines, log, Resource,
+} from '../src';
 
 const sample = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`;
---
+++ __tests__/day20.spec.ts
@@ -0,0 +1,57 @@
+import { getLines } from '../src';
+
+const sample = `1
+2
+-3
+3
+-2
+0
+4`;
+
+interface Value { value: number; }
+
+function mix(original: Value[], copy: Value[]) {
+  const mod = copy.length - 1;
+  original.forEach((value) => {
+    const index = copy.indexOf(value);
+    // Remove
+    copy.splice(index, 1);
+    // Put it back - important to use the copy length because we just
+    // removed one.
+    copy.splice((index + value.value) % mod, 0, value);
+  });
+  return copy;
+}
+
+function part1(values: number[]) {
+  const objectified = values.map((value) => ({ value }));
+  const copy = mix(objectified, [...objectified]);
+  const zero = copy.findIndex((v) => v.value === 0);
+  return [1000, 2000, 3000]
+    .map((i) => copy[(zero + i) % values.length].value)
+    .reduce((acc, v) => acc + v, 0);
+}
+
+function part2(values: number[]) {
+  const objectified = values.map((v) => ({ value: v * 811589153 }));
+  const copy = [...objectified];
+  for (let i = 0; i < 10; i += 1) {
+    mix(objectified, copy);
+  }
+  const zero = copy.findIndex((v) => v.value === 0);
+  return [1000, 2000, 3000]
+    .map((i) => copy[(zero + i) % values.length].value)
+    .reduce((acc, v) => acc + v, 0);
+}
+
+describe('day 20', () => {
+  test('sample data', () => {
+    const lines = sample.split('\n').map(Number);
+    expect(part1(lines)).toEqual(3);
+    expect(part2(lines)).toEqual(1623178306);
+  });
+
+  const lines = getLines('day20.txt').map(Number);
+  test.todo(`Part 1 ${part1(lines)}`);
+  test.todo(`Part 2 ${part2(lines)}`);
+});
